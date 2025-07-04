e7a16ce292c2f04da680bdae1dc927ad905df741
Yaseen <121yaseen@gmail.com>
2025-07-04 18:42:33 +0530

    lets see which will get build and which is staging

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@/lib/supabase/server'
 import { NextRequest, NextResponse } from 'next/server'
 
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
